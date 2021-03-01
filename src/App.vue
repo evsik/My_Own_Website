5d4882f5f759a3709824d46b5a7351632b324b9f
evsik <evsevsikov@gmail.com>
2021-03-01 13:22:36 +0300

    npm run build

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <template>
   <div id="app">
-    <!--    <div id="nav">-->
-    <!--      <router-link to="/">Home</router-link> |-->
-    <!--      <router-link to="/about">About</router-link>-->
-    <!--    </div>-->
-
-    <!--    <Header>-->
-    <!--      <nav class="b-headerHav" slot="nav">-->
-    <!--        <ul class="b-headerHav__ul">-->
-    <!--          <li class="b-headerHav__list">-->
-    <!--            <router-link to="/" class="b-headerHav__link">Works</router-link>-->
-    <!--          </li>-->
-    <!--          <li class="b-headerHav__list">-->
-    <!--            <router-link to="/" class="b-headerHav__link">More</router-link>-->
-    <!--          </li>-->
-    <!--        </ul>-->
-    <!--      </nav>-->
     <!--    </Header>-->
+    <!--    the transition menu is located in the head of the components-->
     <router-view/>
-
-    <!--    <footer/>-->
   </div>
 </template>
 
@@ -29,14 +12,24 @@ import Header from "@/components/Header";
 export default {
   name: "App",
 
+  data() {
+    return {
+      mediaQuery: window.matchMedia('(pointer:coarse)')
+    }
+  },
+
   components: {
     Header
   },
 
+  // mounted() {
+  //   this.mediaQuery.addListener(this.handleTabletChange)
+  //   this.handleTabletChange(this.mediaQuery)
+  // },
+
   updated() {
     this.customStyle()
     this.overStyle()
-    // this.getOverflow()
   },
 
   //This method will be needed in the future to change the header style when switching the router
@@ -54,7 +47,16 @@ export default {
       } else {
         document.querySelector("body").style.overflow = "hidden";
       }
-    }
+    },
+// if the video is not viewed from a computer the background video is disabled
+//     handleTabletChange(e) {
+//       if (e.matches) {
+//         document.querySelector("video").style.display = "none";
+//         console.log('Media Query Matched!')
+//       } else {
+//         document.querySelector("video").style.display = "block";
+//       }
+//     }
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="landing-page">
-    <video src="../assets/first.mp4" autoplay loop muted></video>
+  <div class="landing-page" :class="{ bgcMedia: !mediaActive }">
+    <video src="../assets/first.mp4" autoplay loop muted v-if="mediaActive"></video>
     <div class="wrapper">
       <div class="container">
         <Header/>
@@ -20,10 +20,31 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data() {
+    return {
+      mediaQuery: window.matchMedia('(pointer:coarse)'),
+      mediaActive: false
+    }
+  },
+
+  mounted() {
+    this.mediaQuery.addListener(this.handleTabletChange)
+    this.handleTabletChange(this.mediaQuery)
+  },
+
+  methods: {
+    handleTabletChange(e) {
+      this.mediaActive = !e.matches;
+      document.querySelector("video").style.display = "none";
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
+.bgcMedia {
+  background: url("../assets/pexels-giovanni-calia-2733337.jpg") no-repeat center;
+  background-size: cover;
+}
 </style>
